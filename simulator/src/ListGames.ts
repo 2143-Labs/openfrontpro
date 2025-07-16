3cc37b09e250257859223185ed40b500953744e5
John Schmidt <john@john2143.com>
2025-07-16 04:30:17 -0400

    got running

--- simulator/src/ListGames.ts
+++ simulator/src/ListGames.ts
@@ -1,1 +1,15 @@
 
+import { Pool } from "pg";
+
+
+let p = new Pool({
+    port: 5432,
+    host: "localhost",
+    user: "postgres",
+    database: "openfrontpro",
+});
+
+await p.connect();
+
+let res = await p.query("SELECT * FROM lobbies;");
+console.log("Lobbies: ", res.rows);
