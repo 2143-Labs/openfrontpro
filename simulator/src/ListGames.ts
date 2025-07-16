5fcf983ca5207f2c39b5de30768a4873da32aa9f
John Schmidt <john@john2143.com>
2025-07-16 04:37:09 -0400

    Add boilerplate

--- simulator/src/ListGames.ts
+++ simulator/src/ListGames.ts
@@ -1,4 +1,5 @@
 
+import { GameRecord, GameRecordSchema } from "openfront-client/src/core/Schemas";
 import { Pool } from "pg";
 
 
@@ -11,5 +12,22 @@ let p = new Pool({
 
 await p.connect();
 
-let res = await p.query("SELECT * FROM lobbies;");
-console.log("Lobbies: ", res.rows);
+let res = await p.query("SELECT * FROM finished_games LIMIT 3;");
+console.log("Games: ", res.rows);
+
+
+for(let game of res.rows) {
+    console.log("Game ID: ", game.game_id);
+    let r = game.result_json;
+    let game: GameRecord = GameRecordSchema.parse(r);
+
+    console.log("Game Record: ", game.info.winner);
+    await simgame(game.game_id, game);
+}
+
+
+async function simgame(gameId: string, record: GameRecord) {
+    // TODO
+
+
+}
