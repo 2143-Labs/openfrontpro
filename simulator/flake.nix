1b8cfd919647928ae3b408aee2f055c0b5b23471
John Schmidt <john@john2143.com>
2025-07-20 17:57:20 -0400

    temp

--- simulator/flake.nix
+++ simulator/flake.nix
@@ -13,11 +13,14 @@
         bun2nix = inputs.bun2nix;
         devShells.default = pkgs.mkShell {
           buildInputs = [
-            pkgs.bun
+            pkgs.nodejs_24
             pkgs.nodePackages.typescript-language-server
             pkgs.nodePackages.eslint
+            pkgs.nodePackages.typescript
+            pkgs.pnpm_10
+            packages.my-husky
             # Add the bun2nix binary to our devshell
-            bun2nix.packages.${system}.default
+            #bun2nix.packages.${system}.default
           ];
 
           shellHook = ''
@@ -26,12 +29,63 @@
           '';
         };
 
-        packages.default = pkgs.callPackage ./. {
+        packages.my-husky = pkgs.writeShellScriptBin "husky" ''
+          echo "Dummy Husky"
+        '';
+
+        packages.simulator-bun = pkgs.callPackage ./. {
           inherit (bun2nix.lib.${system}) mkBunDerivation;
           inherit pkgs;
         };
 
-        packages.simulator = packages.default;
+        packages.default = packages.simulator;
+        packages.simulator-node = pkgs.buildNpmPackage {
+          pname = "openfront-simulator";
+          version = "0.1.0";
+          buildInputs = [
+            pkgs.nodejs_24
+            packages.my-husky
+          ];
+
+          src = ./.;
+          npmDeps = pkgs.importNpmLock {
+            npmRoot = ./.;
+          };
+          npmConfigHook = pkgs.importNpmLock.npmConfigHook;
+
+          installPhase = ''
+            mkdir -p $out
+            cp -r src/* $out/
+          '';
+        };
+
+        packages.simulator = pkgs.stdenv.mkDerivation (finalAttrs: {
+          pname = "openfront-simulator";
+          name = "openfront-simulator";
+          src = ./.;
+          version = "0.1.0";
+          buildInputs = [
+            pkgs.nodejs_24
+            pkgs.pnpm_10
+            packages.my-husky
+          ];
+          nativeBuildInputs = [
+            #pkgs.makeWrapper
+            pkgs.pnpm_10.configHook
+          ];
+          pnpmDeps = pkgs.pnpm_10.fetchDeps {
+            inherit (finalAttrs) pname version src;
+            hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+          };
+
+
+          #installPhase = ''
+            #mkdir -p $out/bin
+            #cp ${packages.simulator-node}/bin/openfront-simulator $out/bin/openfront-simulator
+            #wrapProgram $out/bin/openfront-simulator \
+              #--set RUST_LOG info
+          #'';
+        });
       }
     );
 }
