e0e97ebbb2b386c0be83cd3b233c6936550573a7
John Schmidt <john@john2143.com>
2025-07-20 14:47:13 -0400

    don't log health checks

--- rust/src/middleware.rs
+++ rust/src/middleware.rs
@@ -14,6 +14,10 @@ impl<B> OnRequest<B> for LogOnRequest {
     fn on_request(&mut self, request: &Request<B>, _span: &Span) {
         let method = request.method().clone();
         let path = request.uri().path().to_string();
+        if path == "/health" {
+            // Don't log health checks
+            return;
+        }
 
         //let ts_formatted = ts_formatted();
         let _ = _span.enter();
@@ -30,18 +34,18 @@ pub struct LogOnResponse;
 impl<B> OnResponse<B> for LogOnResponse {
     fn on_response(
         self,
-        response: &axum::http::Response<B>,
-        latency: std::time::Duration,
+        _response: &axum::http::Response<B>,
+        _latency: std::time::Duration,
         _span: &Span,
     ) {
-        let _ent = _span.enter();
-        let status = response.status();
-        //let ts_formatted = ts_formatted();
+        //let _ent = _span.enter();
+        //let status = response.status();
+        ////let ts_formatted = ts_formatted();
 
-        let us = latency.as_micros();
-        let ms = (us / 100) as f32 / 10.0;
-        // Log also the method and path from the prev
-        // Use tracing to get the span value
-        info!(">{status} ({ms}ms)",);
+        //let us = latency.as_micros();
+        //let ms = (us / 100) as f32 / 10.0;
+        //// Log also the method and path from the prev
+        //// Use tracing to get the span value
+        //info!(">{status} ({ms}ms)",);
     }
 }
