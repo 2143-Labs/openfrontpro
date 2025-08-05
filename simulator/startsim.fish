4ac27bb8b9dcd3c5e329feeb101f828c5b0ab7cb
John Schmidt <john@john2143.com>
2025-08-05 16:17:48 -0400

    Add startsim

--- simulator/startsim.fish
+++ simulator/startsim.fish
@@ -1,3 +1,4 @@
-for x in (seq 10);
-    fish -c "while true do; RUN_CLIENT=true npm start >job$x.txt 2>&1; sleep 5; end" &;
+#!/usr/bin/env fish
+for x in (seq 3);
+    fish -c "while true do; RUN_CLIENT=true npm start >>job$x.txt 2>&1; sleep 5; end" &;
 end
