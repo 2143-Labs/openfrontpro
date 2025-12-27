9d9ad121dff21c51eb3a0565630340a6c5f563b9
John Schmidt <john@john2143.com>
2025-12-27 18:19:10 -0500

    Add changes for latest openfront

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     #bun2nix.url = "github:baileyluTCD/bun2nix";
 
     openfrontio_cur = {
-        url = "github:OpenFrontIO/OpenFrontIO/v0.25.6";
+        url = "github:OpenFrontIO/OpenFrontIO/v0.28.2";
         flake = false;
     };
 
